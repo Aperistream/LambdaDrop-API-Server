b7cb3c58656da9483e787bf9eeba9dd48d162bb6
Le_Mocha <60754513+LeMocha@users.noreply.github.com>
2021-12-07 17:49:00 +0400

    Travail Final
    - Remplacement de sqlite3 par mysql.
    - Retrait de FS
    - Sécurisation des accès
    - Optimisation de la logique de fonctionnement

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # LambdaDrop-API-Server
+
 Le serveur qui va établir les communications entre le gérant de stream et la preview.
+
+Ce serveur nécessite d'être connecté à la base de données du serveur de gestion des accès.
+
+### 💻 Pré-requis :
+
+* Connexion à la base de données MyAperistream
+* Node.js 14+
+* 512Mo RAM
+
+### 🔧 Installation :
+
+Pour installer le serveur, une simple commande est suffisante.
+
+```bash
+cd installdir && npm i
+```
+
+### ⚙️ Configuration :
+
+Vous pouvez personaliser le port de connexion ainsi que les identifiants de la base de données dans le fichier `.env` dans le dossier racine du serveur comme dans l'exemple ci-dessous :
+
+```.env
+PORT=40469
+DB="USER:PASSWORD@SERVER:PORT/DATABASE"
+```
+
+### 📌 Mise en Production :
+
+```bash
+npm run start
+```
+
--- main.js
+++ main.js
@@ -1,67 +1,118 @@
-const sqlite3 = require('sqlite3').verbose();
-var express = require('express');
-const fs = require('fs');
-var server = express();
+// Dépendances
+var mysql = require('mysql');
+var express = require('express')
+var server = require('express')();
+require('dotenv').config()
 
-let db = new sqlite3.Database('./tokens.db', (err) => {
-    if (err) throw err;
-    console.log('Connected to the local database.');
+// Connexion à la database
+console.log("🔗 Connexion à la base myAperistream.")
+var pool = mysql.createPool({
+    "host": process.env.DB_HOST,
+    "user": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_DATA
 });
+
+// Le stockage des parties se trouvent dans la mémoire temporaire du prog. Elle doit dont être définie à chaque boot du serveur.
+let dataparty = {"team1":{"q1":{"th1":{"thname":"","question":"","rep1":"","rep2":"","rep3":"","rep4":"","correp":1},"th2":{"thname":"","question":"","rep1":"","rep2":"","rep3":"","rep4":"","correp":1}},"q2":{"th1":{"thname":"","question":"","rep1":"","rep2":"","rep3":"","rep4":"","correp":1},"th2":{"thname":"","question":"","rep1":"","rep2":"","rep3":"","rep4":"","correp":1}},"q3":{"th1":{"thname":"","question":"","rep1":"","rep2":"","rep3":"","rep4":"","correp":1},"th2":{"thname":"","question":"","rep1":"","rep2":"","rep3":"","rep4":"","correp":1}},"q4":{"th1":{"thname":"","question":"","rep1":"","rep2":"","rep3":"","rep4":"","correp":1},"th2":{"thname":"","question":"","rep1":"","rep2":"","rep3":"","rep4":"","correp":1}}},"team2":{"q1":{"th1":{"thname":"","question":"","rep1":"","rep2":"","rep3":"","rep4":"","correp":1},"th2":{"thname":"","question":"","rep1":"","rep2":"","rep3":"","rep4":"","correp":1}},"q2":{"th1":{"thname":"","question":"","rep1":"","rep2":"","rep3":"","rep4":"","correp":1},"th2":{"thname":"","question":"","rep1":"","rep2":"","rep3":"","rep4":"","correp":1}},"q3":{"th1":{"thname":"","question":"","rep1":"","rep2":"","rep3":"","rep4":"","correp":1},"th2":{"thname":"","question":"","rep1":"","rep2":"","rep3":"","rep4":"","correp":1}},"q4":{"th1":{"thname":"","question":"","rep1":"","rep2":"","rep3":"","rep4":"","correp":1},"th2":{"thname":"","question":"","rep1":"","rep2":"","rep3":"","rep4":"","correp":1}}}}
+let optparty = {"scores":{"show":false,"team1":100,"team2":100},"state":{"status":0,"question":0,"q-theme":1,"team":1}}
+let repmonn = {"1":0,"2":0,"3":0,"4":0}
+
+server.use(express.json()) // for parsing application/json
+server.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 
 server.get('/', function(req, res) {
-    res.status(200).send("Félicitations ! Si tu trouves cette page, soit tu es du staff soit tu n'as rien à foutre ici mais tu es là ! Alors envoies moi ton CV à contact@manoah.fr")
+    res.status(200).json({version:"Beta 0.8.1",author:"Le_Mocha",project_name:"LambdaDropServer"})
 });
 
-server.get('/about', function(req, res){
-    res.status(200).json({version:"Alpha 0.1",Author:"Le_Mocha",Project_Name:"LambdaDropServer"})
-});
+server.route('/party')
+.get(function(req, res){
 
-server.get('/party', function(req, res){
-    fs.readFile("./data/party.json", 'utf-8', (err, data) => {
-        if(err){
-            res.status(500).send(err)
-            return;
-        }
-        res.status(200).json(JSON.parse(data))
-    });
-})
-
-server.get('/update', (req, res) => {
     var token = req.query.t;
 
-    db.all(`SELECT * FROM tokens WHERE t = "${token}"`, [], (err, result) => {
-        if (err){
-            res.status(500).send(err.message) 
-            return
+    pool.getConnection(function(err, connection){
+
+        if(err){
+            res.status(500).send('SQL ERROR : '+err.message)
+            return console.log(err);
         }
-        
-        if (!result[0]) {
-            res.status(403).send("403 Error : Forbidden")
-            return
-        } else {
-            try {
-                JSON.parse(req.query.p)
-            } catch (err){
-                res.status(500).send(err.message)
+        connection.query(`SELECT * FROM tokens WHERE t = "${token}" AND (perm = 1 OR perm = 2)`, function(err, result){
+            connection.release();
+            if (err){
+                res.status(500).send(err.message) 
                 return
             }
-        
-            fs.writeFile('./data/party.json', req.query.p, function (err) {
-                if (err){
-                    res.status(500).send(err)
+            if (!result[0]) {
+                res.status(403).send("403 Error : Forbidden")
+                return
+            } else {
+
+                res.status(200).json({
+                    party: dataparty,
+                    options: optparty,
+                    traps: repmonn
+                })
+                
+            }
+        });
+    });
+
+})
+.post(function(req,res){
+
+    var token = req.query.t;
+
+    pool.getConnection(function(err, connection){
+
+        if(err){
+            res.status(500).send('SQL ERROR : '+err.message)
+            return console.log(err);
+        }
+        connection.query(`SELECT * FROM tokens WHERE t = "${token}" AND (perm = 1 OR perm = 2)`, function(err, result){
+            connection.release();
+            if (err){
+                res.status(500).send(err.message) 
+                return
+            }
+            if (!result[0]) {
+                res.status(403).send("403 Error : Forbidden")
+                return
+            } else {
+                try {
+                    res_infos = req.body
+                } catch (err){
+                    res.status(500).send(err.message)
                     return
                 }
-            });
-        
-            res.status(200).json("nice")
-        }
-    })
+                
+                if(req.query.p == "pty") {
+                    dataparty = res_infos
+                }
+                else if(req.query.p == "opt"){
+                    optparty = res_infos
+                }
+                else if (req.query.p == "mon"){
+                    repmonn = res_infos
+                }
+                else if (repmonn.query.p == "scores"){
+                    optparty.scores.show = true
+                    setTimeout( function(){ optparty.scores.show = false }, 5000)
+                }
+                else {
+                    res.status(400).send("400 Bad Request : Data Type Not Defined.")
+                }
+                res.status(200).send('Updated')
+            }
+        });
+    });
+
 });
 
 server.get('/ping', function(req, res) {
-    res.status(200).send("pong !")
+    res.status(200).send("pong")
 });
 
-server.listen(40469, () => {
-    console.log("Serveur démarré sur le port 40469")
+server.listen(process.env.PORT, () => {
+    console.log(`🌍 Serveur démarré sur le port ${process.env.PORT}`)
 })
